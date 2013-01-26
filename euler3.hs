bf0ae8960baa23f2be3c708f1b0f4a0dc05abf7b
Raveline <root@sd-15896.dedibox.fr>
2013-01-26 18:17:25 +0100

    Compiling ! (And, if I am to believe my tests, working).  But not able to proceed the real euler3 problem (most likely a memory issue).

--- euler3.hs
+++ euler3.hs
@@ -1,18 +1,17 @@
 getFirstPrimeFactor :: Int -> Int
-getFirstPrimeFactor x = sieves(factors(x) x)
-	where
-		factors :: Int -> [Int]
-		factors n = [x|x <- [floor(sqrt(fromIntegral(n)))..2], n `mod` x == 0]
-		
-		sieves :: Int -> [Int] -> Int
-		sieves n [] = 0
-		sieves n [x] = x
-		sieves n (x:xs) =	if length(withoutFactor) == length(xs)
-						then x
-						else sieves n/x withoutFactor
-					where
-						withoutFactor = filter (`mod` x == 0) xs
+getFirstPrimeFactor x = last(sieves x (factors x))
+    where
+        factors :: Int -> [Int]
+        factors n = [x|x <- [2.. firstPotentialFactor], n `mod` x == 0]
+            where firstPotentialFactor = n `div` 2
 
+        sieves :: Int -> [Int] -> [Int]
+        sieves n [] = []
+        sieves n [x] = [x]
+        sieves n (x:xs) = x:sieves n (filter(isNotFactorOf x) xs)
+            where
+                isNotFactorOf :: Int -> Int -> Bool
+                isNotFactorOf f n = n `mod` f /= 0
 
-euler3 :: [Int]
+euler3 :: Int
 euler3 = getFirstPrimeFactor 600851475143
