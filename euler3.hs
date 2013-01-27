b7941481b0c07fe847665e3f513c1a42022176a3
Raveline <root@sd-15896.dedibox.fr>
2013-01-27 13:18:00 +0100

    Better solution.

--- euler3.hs
+++ euler3.hs
@@ -1,17 +1,9 @@
-getFirstPrimeFactor :: Int -> Int
-getFirstPrimeFactor x = last(sieves x (factors x))
-    where
-        factors :: Int -> [Int]
-        factors n = [x|x <- [2.. firstPotentialFactor], n `mod` x == 0]
-            where firstPotentialFactor = n `div` 2
-
-        sieves :: Int -> [Int] -> [Int]
-        sieves n [] = []
-        sieves n [x] = [x]
-        sieves n (x:xs) = x:sieves n (filter(isNotFactorOf x) xs)
-            where
-                isNotFactorOf :: Int -> Int -> Bool
-                isNotFactorOf f n = n `mod` f /= 0
+reduce :: Int -> Int
+reduce n = if null(facts)
+            then n 
+            else reduce (n `div` head facts)
+           where 
+            facts = take 1 [x|x <- [2.. n `div` 2], n `mod` x == 0]
 
 euler3 :: Int
-euler3 = getFirstPrimeFactor 600851475143
+euler3 = reduce 600851475143
