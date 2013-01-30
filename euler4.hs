b2e6f3ee271fe0c2fdce6ca96a4a4dd4f31b4dcc
Raveline <root@sd-15896.dedibox.fr>
2013-01-30 09:53:01 +0100

    Naive implementation.

---
+++ euler4.hs
@@ -0,0 +1,15 @@
+listPalindromes :: [Int]
+listPalindromes = euler4' 999 999
+    where
+        euler4' :: Int -> Int -> [Int]
+        euler4' x y 
+            | isPalindrome (x*y) = (x*y):euler4' (x-1) y
+            | y == 100 = []
+            | x > 99 = euler4' (x-1) y
+            | otherwise = euler4' 999 (y-1)
+        
+        isPalindrome :: Int -> Bool
+        isPalindrome n = show n == reverse (show n) && length(show n) > 1
+
+euler4 :: Int
+euler4 = maximum(listPalindromes)
